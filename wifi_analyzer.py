3bb48178bd45921f3fddc0d75227d9ec6edf3892
darek <darek@cosmose.co>
2019-01-17 17:05:36 +0100

    Added key handlers, refactoring

--- wifi_analyzer.py
+++ wifi_analyzer.py
@@ -1,14 +1,15 @@
+import os
 import sys
 import pygame
 import threading
 import time
 
 from iw_wrapper import Iw
+from options_menu import OptionsMenu
+from color import Color
 
 '''
 Bottom buttons: scan, reset, menu, histogram / chart
-
-Display: region, is continous scan, scan method
 
 Menu:
  - set region: list with 6-8 ico codes
@@ -17,22 +18,19 @@ Menu:
  - continous scan on/off
 '''
 
-class Color:
-	defaultLight = (192, 200, 198)
-	defaultDark = (29, 31, 33)
-	black = (0, 0, 0)
-	yellow = (240, 198, 116)
-	teal = (94, 141, 135)
-
 class WifiAnalyzer():
 
 	iw = Iw()
+	optionsMenu = None
 
 	isFullScreen = False
 	font = None
 	screen = None
 
 	isChart = True
+	isContinusScan = False
+	isActiveScan = True
+	isMenuDisplayed = False
 
 	def createScreen(self):
 		pygame.init()
@@ -42,9 +40,11 @@ class WifiAnalyzer():
 			size = pygame.display.list_modes()[0]
 			self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 		else:
+			os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (1000,800)
 			self.screen = pygame.display.set_mode((320,240))
 		
 		self.erase()
+		self.optionsMenu = OptionsMenu()
 
 	def drawHeader(self):
 		padding = 5
@@ -58,13 +58,19 @@ class WifiAnalyzer():
 		self.screen.blit(self.font.render("Region", 1, Color.defaultDark), (13, 29))
 		self.screen.blit(self.font.render(self.iw.region, 1, Color.yellow), (68, 29))
 
-	def drawMenu(self):
-		self.drawMenuItem("SCAN", 0)
-		self.drawMenuItem("RSET", 79)
-		self.drawMenuItem("MENU", 158)
-		self.drawMenuItem("HIST" if self.isChart else "CHRT", 236)
+		self.screen.blit(self.font.render("Continuous", 1, Color.defaultDark), (100, 29))
+		self.screen.blit(self.font.render("YES" if self.isContinusScan else "NO", 1, Color.yellow), (190, 29))
 
-	def drawMenuItem(self, text, leftOffset):
+		scanMethodOffset = 254 if self.isActiveScan else 246
+		self.screen.blit(self.font.render("Active" if self.isActiveScan else "Passive", 1, Color.yellow), (scanMethodOffset, 29))
+
+	def drawBottomMenu(self):
+		self.drawBottomMenuItem("SCAN", 0)
+		self.drawBottomMenuItem("RSET", 79)
+		self.drawBottomMenuItem("MENU", 158)
+		self.drawBottomMenuItem("HIST" if self.isChart else "CHRT", 236)
+
+	def drawBottomMenuItem(self, text, leftOffset):
 		pygame.draw.rect(self.screen, Color.yellow, [15 + leftOffset, 220, 50, 18])
 		label = self.font.render(text, 1, Color.defaultDark)
 		self.screen.blit(label, (23 + leftOffset, 221))
@@ -92,10 +98,28 @@ class WifiAnalyzer():
 		threading.Thread(target=self.iw.loadRegion).start()
 
 	def loop(self):
-		i = 0
 		while True:
 
 			for event in pygame.event.get():
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_ESCAPE:
+						pygame.quit()
+						quit()
+					if event.key == pygame.K_0:
+						# Scan
+						a = 0
+					if event.key == pygame.K_1:
+						# Reset
+						b = 0
+
+					if event.key == 51:
+						# Menu
+						self.isMenuDisplayed = not self.isMenuDisplayed 
+						
+					if event.key == 52:
+						# Chart type
+						d = 0
+							
 				if event.type == pygame.QUIT:
 				    pygame.quit()
 				    quit()
@@ -103,13 +127,18 @@ class WifiAnalyzer():
 			self.erase()
 			self.drawBorder()
 			self.drawHeader()
-			self.drawMenu()
+			self.drawBottomMenu()
 			self.drawInfo()
 
-			pygame.draw.lines(self.screen, (80,0,0), False, [(i,100), (150,200), (200,100)], 1)
-			pygame.display.update()
+			if self.isMenuDisplayed:
+				self.optionsMenu.displayMenu(self.screen)
+			else:
+				# display chart
+				pygame.draw.lines(self.screen, (80,0,0), False, [(10,100), (150,200), (200,100)], 1)
+			
 			pygame.time.delay(50)
-			i = i + 1
+			pygame.display.update()
+
 
 wifiAnalyzer = WifiAnalyzer()
 wifiAnalyzer.createScreen()
